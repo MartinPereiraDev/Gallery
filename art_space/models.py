cc88982050cf7b12ecc59c25fee8b9fc10150331
Martin Pereira <tin900@hotmail.com>
2023-11-29 22:14:43 -0300

    get and put

--- art_space/models.py
+++ art_space/models.py
@@ -4,9 +4,9 @@ from django.core.exceptions import ValidationError
 
 from django.contrib.auth import get_user_model
 
-
-
 User = get_user_model()
+
+
 
 # validate files work (jpg, png)   
 def validate_file_image(value):
